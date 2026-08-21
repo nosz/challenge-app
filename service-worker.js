96a44be6e76e646ed63898562e1bf0f1dd5b7bb9
nosz <nosz.mail@web.de>
2026-08-21 14:50:11 +0200

    change los g...

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Service Worker für Challenge App
 // Cache-Name bei jeder neuen Version hochzählen (z.B. wenn sich index.html ändert),
 // damit Nutzer die neue Version bekommen statt einer alten aus dem Cache.
-const CACHE_VERSION = '5.5.3';
+const CACHE_VERSION = '5.6.0';
 const CACHE_NAME = 'challenge-app-' + CACHE_VERSION;
 
 const ASSETS_TO_CACHE = [
